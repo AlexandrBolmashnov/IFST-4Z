d091ef49712aa761d741920371bba94509563f54
AlexandrBolmashnov <97880465+AlexandrBolmashnov@users.noreply.github.com>
2022-06-16 12:57:09 +0300

    Add files via upload

---
+++ boobs1.java
@@ -0,0 +1,55 @@
+package com.pr1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+
+ */
+@WebServlet("/boobs1")
+public class boobs1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public boobs1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("<html>"); 
+		out.println("<head>"); 
+		out.println("<title> boobs</title>"); 
+		out.println("</head>"); 
+		out.println("<body>"); 
+		out.println("<h1>Reservation</h1>");
+		out.println(request.getParameter("Name") + "'s You has been reserved."); 
+		out.println("</body>"); 
+		out.println("</html>");	
+			
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
